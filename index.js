412339feae12030b3e995964894d9a8487f530cd
Sune Seifert <sse@abtion.com>
2024-08-20 12:36:01 +0200

    Modernise code

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 import { readdirSync, statSync, writeFileSync } from "fs";
 
-function filenamesToJSON({ path, output, filesKey, indentlevel = 0 }) {
+function filenamesToJSON({
+  path = "./",
+  output = "./files.json",
+  filesKey = "files",
+  indentlevel = 0,
+}) {
   output = forceJSON(output);
-  path = path || "./";
-  filesKey = filesKey || "files";
-  var obj = {};
-  obj[filesKey] = [];
-  var outputName = output.split("/");
-  outputName = outputName[outputName.length - 1];
+  const obj = { [filesKey]: [] };
+  const outputName = output.split("/").pop();
   createJSON(path, obj);
+
   function createJSON(dir, obj) {
-    var files = readdirSync(dir);
+    const files = readdirSync(dir);
     files.forEach((file) => {
-      var name = `${dir}/${file}`;
+      const name = `${dir}/${file}`;
       if (statSync(name).isDirectory()) {
         createJSON(name, addDirectory(obj, name, filesKey));
-      } else if (file != outputName) {
+      } else if (file !== outputName) {
         obj[filesKey].push(file);
       }
     });
   }
-  var jsonObj = JSON.stringify(obj, null, indentlevel);
+
+  const jsonObj = JSON.stringify(obj, null, indentlevel);
   try {
     writeFileSync(output, jsonObj);
   } catch (error) {
@@ -28,25 +31,23 @@ function filenamesToJSON({ path, output, filesKey, indentlevel = 0 }) {
   }
 
   function addDirectory(obj, dir, filesKey) {
-    var expl = dir.split("/");
-    var lastItem = expl.length - 1;
-    var dirName = expl[lastItem] == "" ? expl[lastItem - 1] : expl[lastItem];
-    obj[dirName] = {};
-    obj[dirName][filesKey] = [];
+    const expl = dir.split("/");
+    const dirName = expl[expl.length - 1] || expl[expl.length - 2];
+    obj[dirName] = { [filesKey]: [] };
     return obj[dirName];
   }
+
   function forceJSON(filename) {
     if (!filename) {
       return "./files.json";
     }
-    var ext = filename.split(".").pop();
+    const ext = filename.split(".").pop();
     if (ext === "json") {
       return filename;
     }
-    if (filename[filename.length - 1] !== "/") {
-      filename += "/";
-    }
-    return filename + "files.json";
+    return filename.endsWith("/")
+      ? `${filename}files.json`
+      : `${filename}/files.json`;
   }
 }
 
